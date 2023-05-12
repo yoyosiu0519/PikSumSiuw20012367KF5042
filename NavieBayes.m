2d37ff6ad31f0a1d2acb3b21e5db33c1278a1780
Yoyo <124893353+yoyosiu0519@users.noreply.github.com>
2023-05-12 03:33:24 +0100

    MATLAB ASSIGNMENT
    Student ID: w20012367

---
+++ NavieBayes.m
@@ -0,0 +1,24 @@
+clc;
+clear;
+%Fetch file
+filename = "spambase.csv";
+%Extract table
+dataTable = readtable(filename);
+TableMatrix = table2array(dataTable);
+[numRow,numCol] = size(TableMatrix);
+ind = randperm(numRow);
+TableMatrix = TableMatrix(ind,:);
+%split train and test
+partition = cvpartition(size(TableMatrix,1),'HoldOut',0.2);
+idx = partition.test;
+dataForTest = TableMatrix(idx,:);
+dataForTrain = TableMatrix(~idx,:);
+xTrain = dataForTrain(:,1:57); % Xcoor for training
+yTrain = dataForTrain(:,58);% Ycoor for training
+xTest = dataForTest(:,1:57); % Xcoor for testing
+yTest = dataForTest(:,58);%Ycoor for testing
+
+nbModel = fitcnb(xTrain,yTrain);% the naive bayes
+yPred = predict(nbModel,xTest);
+accuracy = sum(yPred == yTest)/numel(yTest);
+nbChart = confusionchart(yTest,yPred);
